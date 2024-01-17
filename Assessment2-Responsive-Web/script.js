3bb45b4a21b8abc1c30936c350b0bb358744a5c2
Joezxc <113306553+Joezxc@users.noreply.github.com>
2024-01-17 10:59:21 +0400

    Add files via upload

---
+++ Assessment2-Responsive-Web/script.js
@@ -0,0 +1,8 @@
+const navbarToggler = document.querySelector('.navbar__top--toggler');
+
+const navbarCollapse = document.querySelector('.navbar__collapse');
+
+navbarToggler.addEventListener('click', () => {
+ navbarToggler.classList.toggle('change');   
+ navbarCollapse.classList.toggle('show__nav');   
+})
