1a61d37bcf669718cc008856ed0aa553532dc5b2
Joezxc <113306553+Joezxc@users.noreply.github.com>
2024-01-17 10:59:59 +0400

    Delete Assessment2-Responsive-Web directory

--- Assessment2-Responsive-Web/script.js
+++
@@ -1,8 +0,0 @@
-const navbarToggler = document.querySelector('.navbar__top--toggler');
-
-const navbarCollapse = document.querySelector('.navbar__collapse');
-
-navbarToggler.addEventListener('click', () => {
- navbarToggler.classList.toggle('change');   
- navbarCollapse.classList.toggle('show__nav');   
-})
